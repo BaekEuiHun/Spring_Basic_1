5cef0491c12405b328d1f682dd9b55ef50a3f88b
EuiHun_Bori <euihunbaek@gmail.com>
2024-12-06 17:10:50 +0900

    코드정렬

--- src/main/java/hello/core1/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core1/discount/FixDiscountPolicy.java
@@ -4,8 +4,8 @@ import hello.core1.member.Grade;
 import hello.core1.member.Member;
 
 public class FixDiscountPolicy implements DiscountPolicy {
+    
     private int discountFixAmount = 1000; //1000원 할인
-
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
