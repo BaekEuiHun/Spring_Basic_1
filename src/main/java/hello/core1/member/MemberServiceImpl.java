608dba99abf629609f570409aa0a855ed39d7fc3
EuiHun_Bori <euihunbaek@gmail.com>
2025-02-03 22:46:34 +0900

    Update MemberServiceImpl.java

--- src/main/java/hello/core1/member/MemberServiceImpl.java
+++ src/main/java/hello/core1/member/MemberServiceImpl.java
@@ -10,6 +10,7 @@ public class MemberServiceImpl implements MemberService {
 
     }
 
+    
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
