c4756df9eacaddd187e62fe553e83d3ea1ce0b58
BaekEuiHun <euihunbaek@gmail.com>
2025-07-09 21:54:01 +0900

    학습2

--- src/test/java/hello/core1/order/OrderServiceTest.java
+++ src/test/java/hello/core1/order/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package hello.core1.order;
 
+import hello.core1.AppConfig;
 import hello.core1.Order.Order;
 import hello.core1.Order.OrderService;
 import hello.core1.Order.OrderServiceImpl;
@@ -15,11 +16,20 @@ import hello.core1.member.Member;
 import hello.core1.member.MemberService;
 import hello.core1.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
+
 
     @Test
     void createOrder() {
