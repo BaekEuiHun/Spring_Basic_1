a1af919d638c65c4bbd537d6a7579b4746d2ef01
BaekEuiHun <euihunbaek@gmail.com>
2025-07-09 22:08:12 +0900

    appconfig 리팩토링

--- src/main/java/hello/core1/AppConfig.java
+++ src/main/java/hello/core1/AppConfig.java
@@ -3,7 +3,9 @@ package hello.core1;
 
 import hello.core1.Order.OrderService;
 import hello.core1.Order.OrderServiceImpl;
+import hello.core1.discount.DiscountPolicy;
 import hello.core1.discount.FixDiscountPolicy;
+import hello.core1.member.MemberRepository;
 import hello.core1.member.MemberService;
 import hello.core1.member.MemberServiceImpl;
 import hello.core1.member.MemoryMemberRepository;
@@ -11,13 +13,20 @@ import hello.core1.member.MemoryMemberRepository;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(new MemoryMemberRepository(),discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 
 
 }
