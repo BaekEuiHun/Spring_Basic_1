b40c0f5367ee6744503eec63a12637d4378033d6
BaekEuiHun <euihunbaek@gmail.com>
2025-07-15 21:41:04 +0900

    복습11

--- src/main/java/hello/core1/member2/MemberServiceImpl2.java
+++ src/main/java/hello/core1/member2/MemberServiceImpl2.java
@@ -1,11 +1,15 @@
 package hello.core1.member2;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MemberServiceImpl2 implements MemberService2 {
 
     private final MemberRepository2 memberRepository;
 
+    @Autowired
     public MemberServiceImpl2(MemberRepository2 memberRepository) {
         this.memberRepository = memberRepository;
     }
